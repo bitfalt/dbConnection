d89be4cce06018b80f55aa97c47da807ee63d0c6
bitfalt <75431447+bitfalt@users.noreply.github.com>
2023-05-10 15:12:14 -0600

    Add route to use a pool connection

---
+++ REST API/app/routes/collectorCountryPool.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, jsonify, request
+from repositories.collectorCountry_repository import getCollectorCountry
+
+# Create blueprint (route)
+bp = Blueprint('collectorCountryPool', __name__, url_prefix='/collectorCountryPool')
+
+@bp.route('/', methods=['POST'])
+
+def getCollectorByCountry():
+    # Get country from request
+    country = request.json['country']
+    # Get result from request
+    result = getCollectorCountry(True, country)
+    return jsonify([dict(row) for row in result])
