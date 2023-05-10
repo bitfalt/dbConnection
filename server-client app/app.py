8b4c63c41afb482df3c9bd3e469194ba8ca3a87b
bitfalt <75431447+bitfalt@users.noreply.github.com>
2023-05-10 07:57:39 -0600

    Finish UI design

--- server-client app/app.py
+++ server-client app/app.py
@@ -1,11 +1,22 @@
 import tkinter as tk
+from tkinter import ttk
 
+# db variables
 cname = "Esencial Verde"
 pname = "Taco Bell"
 address = "Costa Rica Calle 1, San Juan, PR"
-OPTIONS = ["Chemical Waste", "Nuclear Waste", "Paper Waste", "Organic Waste"]
+WOPTIONS = ["Chemical Waste", "Nuclear Waste", "Paper Waste", "Organic Waste"]
+COPTIONS = ["Suli", "Calvo", "Makers", "Quakers"]
+NUMBERS = []
+
+# create list of numbers from 1 to 100
+for i in range(1, 101):
+    NUMBERS.append(str(i))
+
+
 
 root = tk.Tk()
+root.resizable(False, False)
 root.geometry('800x620')
 
 # set background color
@@ -49,23 +60,71 @@ for i in range(20):
 
 # create widgets to add waste
 
-amountLabel = tk.Label(root, text="Amount:", bg='#f2f2f2', font=('Helvetica', 14, 'bold'))
-amountLabel.place(x=10, y=470)
+WastePickupLabel = tk.Label(root, text="Waste to pickup", bg='#f2f2f2', font=('Helvetica', 16, 'bold'))
+WastePickupLabel.place(x=10, y=460) 
+
+amountLabelW = tk.Label(root, text="Amount:", bg='#f2f2f2', font=('Helvetica', 14))
+amountLabelW.place(x=10, y=495)
 numberWaste = tk.StringVar(root)
 numberWaste.set("")
-numberEntry = tk.Entry(root, textvariable=numberWaste, font=('Helvetica', 14))
-numberEntry.place(x=100, y=470, width=80, height=30)
+
+numberWasteSelection = ttk.Combobox(root, textvariable=numberWaste, values=NUMBERS, font=('Helvetica', 14))
+numberWasteSelection["state"] = "readonly"
+
+numberWasteSelection.pack()
+numberWasteSelection.place(x=100, y=495, width=80, height=30)
+
+# numberEntry = tk.Entry(root, textvariable=numberWaste, font=('Helvetica', 14))
+# numberEntry.place(x=100, y=470, width=80, height=30)
 
 
 wasteType = tk.StringVar(root)
 wasteType.set("Waste Type")
-WasteTypeSelection = tk.OptionMenu(root, wasteType, *OPTIONS)
+
+WasteTypeSelection = ttk.Combobox(root, textvariable=wasteType, values=WOPTIONS, font=('Helvetica', 14))
+WasteTypeSelection["state"] = "readonly"
+
 WasteTypeSelection.pack()
-WasteTypeSelection.place(x=190, y=470, width=150, height=30)
+WasteTypeSelection.place(x=190, y=495, width=180, height=30)
 
-addBtn = tk.Button(root, text="Add", bg='#f2f2f2', font=('Helvetica', 14, 'bold'))
-addBtn.place(x=350, y=470, width=80, height=30)
+addBtnW = tk.Button(root, text="Add", bg='#f2f2f2', font=('Helvetica', 14))
+addBtnW.place(x=380, y=495, width=80, height=30)
+submitBtnW = tk.Button(root, text="Submit", bg='#f2f2f2', font=('Helvetica', 14))
+submitBtnW.place(x=470, y=495, width=80, height=30)
+
+# create widgets to add container
+
+ContainerDeliverLabel = tk.Label(root, text="Containers to deliver", bg='#f2f2f2', font=('Helvetica', 16, 'bold'))
+ContainerDeliverLabel.place(x=10, y=530)
+
+amountLabelC = tk.Label(root, text="Amount:", bg='#f2f2f2', font=('Helvetica', 14))
+amountLabelC.place(x=10, y=560)
+numberContainers = tk.StringVar(root)
+numberContainers.set("")
+
+numberContainersSelection = ttk.Combobox(root, textvariable=numberContainers, values=NUMBERS, font=('Helvetica', 14))
+numberContainersSelection["state"] = "readonly"
+
+numberContainersSelection.pack()
+numberContainersSelection.place(x=100, y=560, width=80, height=30)
 
 
+# numberEntry = tk.Entry(root, textvariable=numberContainers, font=('Helvetica', 14))
+# numberEntry.place(x=100, y=510, width=80, height=30)
+
+
+containerModel = tk.StringVar(root)
+containerModel.set("Containter model")
+
+containerModelSelection = ttk.Combobox(root, textvariable=containerModel, values=COPTIONS, font=('Helvetica', 14))
+containerModelSelection["state"] = "readonly"
+
+containerModelSelection.pack()
+containerModelSelection.place(x=190, y=560, width=180, height=30)
+
+addBtnC = tk.Button(root, text="Add", bg='#f2f2f2', font=('Helvetica', 14))
+addBtnC.place(x=380, y=560, width=80, height=30)
+submitBtnC = tk.Button(root, text="Submit", bg='#f2f2f2', font=('Helvetica', 14))
+submitBtnC.place(x=470, y=560, width=80, height=30)
 
 root.mainloop()
