2a82c4451e4375e5e216e17a572778f5115c303b
bitfalt <75431447+bitfalt@users.noreply.github.com>
2023-05-10 17:28:34 -0600

    Change result format to JSON friendly

--- REST API/app/repositories/collectorCountry_repository.py
+++ REST API/app/repositories/collectorCountry_repository.py
@@ -2,11 +2,15 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.pool import NullPool
 
 def createConnection(pool):
+    username = "user"
+    password = "123456"
+    server = "localhost"
+    database = "esenVerde"
+    driver = "ODBC Driver 17 for SQL Server"
     if pool:
-        engine = create_engine("mysql+pymysql://user:123456@localhost:3306/esenVerde", pool_size=10, max_overflow=0)
+        engine = create_engine(f"mssql+pyodbc://user:123456@localhost:1433/esenVerde?driver={driver}", pool_size=10, max_overflow=0)
     else:
-        engine = create_engine("mysql+pymysql://user:123456@localhost:3306/esenVerde", poolclass=NullPool)
-
+        engine = create_engine(f"mssql+pyodbc://user:123456@localhost:1433/esenVerde?driver={driver}", poolclass=NullPool)
     return engine
 
 def getCollectorCountry(pool, country):
@@ -14,5 +18,9 @@ def getCollectorCountry(pool, country):
     param = country
     query = text("EXEC CollectorsByCountryFiltered :param")
     with engine.connect() as connection:
-        result = connection.execute(query, params={"param": param})
-        return result.fetchall()
+        result = connection.execute(query, {"param": param})
+
+        keys = result.keys()
+        resultDict = [dict(zip(keys, row)) for row in result.fetchall()]
+
+        return resultDict
