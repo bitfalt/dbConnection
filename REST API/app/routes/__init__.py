ce0c5c25382069f53210c08ca254f862f430a0cc
YiWen Liang <gliangw2004@gmail.com>
2023-05-11 14:25:20 -0600

    Add ORM Query route to main app

--- REST API/app/routes/__init__.py
+++ REST API/app/routes/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
-from . import collectorCountryPool, collectorCountryNoPool
+from . import collectorCountryPool, collectorCountryNoPool, ORMQuery
 
 def registerBlueprints(app: Flask):
     app.register_blueprint(collectorCountryPool.bp)
     app.register_blueprint(collectorCountryNoPool.bp)
+    app.register_blueprint(ORMQuery.bp)
