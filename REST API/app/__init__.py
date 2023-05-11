671c10fbedc1ef1520ded9fb1d51f5be1228fcea
YiWen Liang <gliangw2004@gmail.com>
2023-05-11 14:47:01 -0600

    Add db connection and initialization to main app

--- REST API/app/__init__.py
+++ REST API/app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from .routes import registerBlueprints
+from .repositories import db
 
 def createApp():
     app = Flask(__name__)
+    driver = "ODBC Driver 17 for SQL Server"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"mssql+pyodbc://user:123456@localhost:1433/esenVerde?driver={driver}"
+    db.init_app(app)
     # Register blueprints (routes)
     registerBlueprints(app)
     return app
