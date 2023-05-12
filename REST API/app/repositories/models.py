ed6b20dc23334568d7cc66010d21290ec7d7356b
bitfalt <75431447+bitfalt@users.noreply.github.com>
2023-05-12 16:30:03 -0600

    Change class name to match ORM query

--- REST API/app/repositories/models.py
+++ REST API/app/repositories/models.py
@@ -1,43 +1,29 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from flask_sqlalchemy import SQLAlchemy
 
-# create engine and session
-engine = create_engine('mysql+pymysql://user:123456@localhost/esenVerde')
-Session = sessionmaker(bind=engine)
+db = SQLAlchemy()
 
-
-Base = declarative_base()
-
-
-class Collector(Base): 
+class Collector(db.Model): 
     __tablename__ = "Collectors"
     collectorid  = Column(Integer, primary_key=True)
     name = Column(String(30))
 
-class Office(Base):
+class Office(db.Model):
     __tablename__ = "Offices"
     officeid = Column(Integer, primary_key=True)
     name = Column(String(30))
     addressid = Column(Integer, ForeignKey("Addresses.addressid"))
     collectorid = Column(Integer, ForeignKey("Collectors.collectorid"))
 
-class Address(Base):
+class Address(db.Model):
     __tablename__ = "Addresses"
     addressid = Column(Integer, primary_key=True)
     city = Column(String(30))
     street = Column(String(80))
     postalCode = Column(String(20))
-    countryid = Column(Integer, ForeignKey("Countries.countryid"))
+    countryid = Column(Integer, ForeignKey("Country.countryid"))
 
-class Country(Base):
-    __tablename__ = "Countries"
+class Country(db.Model):
+    __tablename__ = "Country"
     countryid = Column(Integer, primary_key=True)
     name = Column(String(30))
-
-
-# create tables
-Base.metadata.create_all(engine)
-
