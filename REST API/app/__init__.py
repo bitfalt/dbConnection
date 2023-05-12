02b479c91fbdab9a5732920c7f0c626d1871b416
bitfalt <75431447+bitfalt@users.noreply.github.com>
2023-05-12 16:29:28 -0600

    Remove DB connection from app

--- REST API/app/__init__.py
+++ REST API/app/__init__.py
@@ -4,9 +4,6 @@ from app.routes import registerBlueprints
 
 def createApp():
     app = Flask(__name__)
-    # driver = "ODBC Driver 17 for SQL Server"
-    # app.config["SQLALCHEMY_DATABASE_URI"] = f"mssql+pyodbc://user:123456@localhost:1433/esenVerde?driver={driver}"
-    # db.init_app(app)
     # Register blueprints (routes)
     registerBlueprints(app)
     return app
